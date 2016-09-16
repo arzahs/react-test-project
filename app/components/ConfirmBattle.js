d780aaa0372a4a7b6edc1185ea6bf09dd13df32a
Sergey Nelepa <nelepa1995@mail.ru>
2016-09-16 16:37:51 +0300

    added battle page

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,42 @@
 var React = require('react');
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+
+function puke(obj){
+	return <pre>{JSON.stringify(obj, null, '')}</pre>
+}
 
 function ConfirmBattle(props){
         return props.isLoading === true 
-          ? <p>Loading!</p>
-          : <p>CONFIRM BATTLE!</p>
+          ? <h1>Loading!</h1>
+          : <div className="jumbotron col-sm-12 text-center">
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <UserDetailsWrapper header='Player 1'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 2'>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
+        </div>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-12'>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
+          </div>
+          <div className='col-sm-12'>
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+    isLoading: React.PropTypes.bool.isRequired,
+    playersInfo: React.PropTypes.array.isRequired,
+    onInitiateBattle: React.PropTypes.func.isRequired
 }
 
 module.exports = ConfirmBattle;
